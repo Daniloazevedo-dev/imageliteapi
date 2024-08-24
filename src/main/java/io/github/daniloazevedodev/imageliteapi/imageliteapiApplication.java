04d2368f1994e05aba487d6bced7d96defb2e66d
Danilo Azevedo <danilo.ann@gmail.com>
2024-08-24 09:12:05 -0300

    Criação da estrutura do projeto

---
+++ src/main/java/io/github/daniloazevedodev/imageliteapi/imageliteapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.daniloazevedodev.imageliteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class imageliteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(imageliteapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/daniloazevedodev/imageliteapi/imageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.daniloazevedodev.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class imageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

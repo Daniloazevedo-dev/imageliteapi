ad1993eb6a9719d5091c27d6fa0a496e5c723a2a
Danilo Azevedo <danilo.ann@gmail.com>
2024-09-22 21:15:20 -0300

    Cirado entidade e configurado conexão com banco de dados postgresql

---
+++ src/main/java/io/github/daniloazevedodev/imageliteapi/domain/enums/ImageExtension.java
@@ -0,0 +1,8 @@
+package io.github.daniloazevedodev.imageliteapi.domain.enums;
+
+public enum ImageExtension {
+    PNG,
+    JPG,
+    JPEG,
+    GIF
+}
--- src/main/java/io/github/daniloazevedodev/imageliteapi/imageliteapiApplication.java
+++ src/main/java/io/github/daniloazevedodev/imageliteapi/imageliteapiApplication.java
@@ -2,8 +2,10 @@ package io.github.daniloazevedodev.imageliteapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class imageliteapiApplication {
 
 	public static void main(String[] args) {
